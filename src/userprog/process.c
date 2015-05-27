b2595892a4b08bf9b57ddf2594c77b72150ecb6d
LeeHyunJae <dubu1993@daum.net>
2015-05-27 17:45:42 +0900

    Revert "first commit"
    This reverts commit ac36cbf6a727d2a3b051d0131696c962e982b75d.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -18,7 +18,6 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
-
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
@@ -42,7 +41,7 @@ process_execute (const char *file_name)
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
-    palloc_free_page (fn_copy);
+    palloc_free_page (fn_copy); 
   return tid;
 }
 
